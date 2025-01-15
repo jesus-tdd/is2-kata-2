baacf46db9126d83533b9db580c81fd1fb1270c4
Jesus <jesus.tdd03@gmail.com>
2025-01-15 12:06:17 +0000

    Changed read() signature

--- src/main/java/software/ulpgc/kata/control/TitleReader.java
+++ src/main/java/software/ulpgc/kata/control/TitleReader.java
@@ -2,8 +2,9 @@ package software.ulpgc.kata.control;
 
 import software.ulpgc.kata.model.Title;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface TitleReader {
-    List<Title> read();
+    List<Title> read() throws IOException;
 }
